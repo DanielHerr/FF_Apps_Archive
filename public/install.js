cccee14b645604e0d0df4b9e62dcfa19c1755280
Daniel Herr <danielherrsoftware@gmail.com>
2025-10-14 14:11:37 -0400

    FFAA beta public. Closes #7.

--- public/install.js
+++ public/install.js
@@ -1,15 +1,52 @@
 "use strict"
 
-install_button.addEventListener("click", function() {
-	if(this.dataset.manifest == "manifest.webapp") {
-		var request = navigator.mozApps.installPackage(location.href + this.dataset.manifest)
-		request.addEventListener("success", function(event) {
-			console.log(event)
+var manifest = install_button.dataset.manifest
+var app
+
+if(navigator.mozApps == undefined) {
+	install_button.disabled = true
+}
+
+if(manifest == "manifest.webapp") {
+	var check = navigator.mozApps.checkInstalled(location.href + manifest)
+	check.addEventListener("success", function() {
+		if(this.result) {
+			app = this.result
+			install_button.textContent = install_button.textContent.replace("Install", "Launch")
+	} })
+} else {
+	var check = navigator.mozApps.getInstalled()
+	check.addEventListener("success", function() {
+		app = this.result.find(function(app) {
+			return app.manifestURL == manifest
 		})
-		request.addEventListener("error", function(event) {
-			console.log(event)
-		})
-	} else {
-		navigator.mozApps.install(this.dataset.manifest)
-	}
+		if(app) {
+			install_button.textContent = install_button.textContent.replace("Install", "Launch")
+} }) }
+check.addEventListener("error", function() {
+	console.error(this.error)
 })
+
+install_button.addEventListener("click", function() {
+	if(install_button.textContent.indexOf("Install") == 0) {
+		if(manifest == "manifest.webapp") {
+			var install = navigator.mozApps.installPackage(location.href + manifest)
+		} else {
+			var install = navigator.mozApps.install(manifest)
+		}
+		install.addEventListener("success", function() {
+			app = this.result
+			install_button.textContent = install_button.textContent.replace("Install", "Launch")
+		})
+		install.addEventListener("error", function() {
+			console.error(this.error)
+			if(this.error.name == "REINSTALL_FORBIDDEN") {
+				install_button.textContent = install_button.textContent.replace("Install", "Launch")
+				install_button.disabled = true
+			} else if(this.error.name != "DENIED") {
+				alert("An error occurred while installing: " + this.error.name)
+			}
+		})
+	} else if(install_button.textContent.indexOf("Launch") == 0) {
+		app.launch()
+} })
