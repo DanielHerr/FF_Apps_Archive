3f87d7b664c49dcf613d2917c8a94ec8c7122794
Daniel Herr <danielherrsoftware@gmail.com>
2025-11-06 19:22:31 -0500

    Enhance hosted apps liveliness classification

---
+++ src/validate_hosted_apps.js
@@ -0,0 +1,40 @@
+import * as Content from "astro:content"
+
+async function check_html(url) {
+	try {
+		let response = await fetch(url)
+		if(response.ok) {
+			let type = response.headers.get("content-type")
+			return type && (type.includes("text/html") || type.includes("application/xhtml+xml"))
+		}
+	} catch {
+		return false
+} }
+
+let apps = await Content.getCollection("metadata", ({ data }) => data.app_type == "hosted")
+let total = apps.length
+let live = [], dead = []
+
+for await(let app of apps) {
+	let manifest = new URL(app.data.manifest_url)
+	try {
+		let response = await fetch(manifest)
+		if(response.ok) {
+			await response.json()
+			live.push(app)
+		} else {
+			throw new Error()
+		}
+	} catch {
+		let parent = new URL(manifest.pathname.split("/").slice(0, -1).join("/"), manifest.origin)
+		let root = new URL("/", manifest.origin)
+
+		if(await check_html(parent)) {
+			live.push(app)
+		} else if(await check_html(root)) {
+			live.push(app)
+		} else {
+			dead.push(app)
+} } }
+
+export { live, dead, total }
