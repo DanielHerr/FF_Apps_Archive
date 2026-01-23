8071db1c9e9f8cec65b77dfe7ca9c9a4ce5b9767
sonofluiz <sonofluiz@gmail.com>
2026-01-23 15:53:12 -0600

    Rafactored icon components

---
+++ src/components/icons/brand/index.js
@@ -0,0 +1,4 @@
+export { default as IconCampfire } from './IconCampfire.astro';
+export { default as IconGithubLogo } from './IconGithubLogo.astro';
+export { default as IconHeaderLogo } from './IconHeaderLogo.astro';
+export { default as IconRocketColor } from './IconRocketColor.astro';
